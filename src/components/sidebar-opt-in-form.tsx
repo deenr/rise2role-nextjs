5f90760f9592f69d65faa4e5d9bb4cf329008b96
Dean Reymen <deenrayman@outlook.com>
2025-02-02 22:50:20 +0300

    feat(sidebar): navigate to share from opt

--- src/components/sidebar-opt-in-form.tsx
+++ src/components/sidebar-opt-in-form.tsx
@@ -1,19 +1,22 @@
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { cn } from '@/lib/utils';
+import Link from 'next/link';
 
 export function SidebarCta({ className, ...props }: React.ComponentPropsWithoutRef<typeof Card>) {
   return (
     <Card className={cn('shadow-none', className)} {...props}>
       <form>
         <CardHeader className="p-4 pb-0">
-          <CardTitle className="text-sm">Ready to streamline your job search?</CardTitle>
+          <CardTitle className="text-sm">Want other people to see your kanban board?</CardTitle>
           <CardDescription>Share your job search in seconds</CardDescription>
         </CardHeader>
         <CardContent className="grid gap-2.5 p-4">
-          <Button className="w-full bg-sidebar-primary text-sidebar-primary-foreground shadow-none" size="sm">
-            Get started
-          </Button>
+          <Link href={'/dashboard/settings?tab=share'}>
+            <Button className="w-full bg-sidebar-primary text-sidebar-primary-foreground shadow-none" size="sm">
+              Get started
+            </Button>
+          </Link>
         </CardContent>
       </form>
     </Card>
